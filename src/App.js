d3938582cacd072d846b002ba090b15f1d5ad9c5
Naima Hassan <naima.hassan434@gmail.com>
2022-05-28 01:49:25 +0100

    Slider- Autp play

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  // Autoplay slide
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => clearInterval(slider);
+  }, [index]);
+
   return (
     <section className="section">
       <div className="title">
