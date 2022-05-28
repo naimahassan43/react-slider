53712b56ac4fb6b66cd713f4777c0188543368b0
Naima Hassan <naima.hassan434@gmail.com>
2022-05-28 01:26:50 +0100

    Slider-Setup Classes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 function App() {
-  const [people, setPeople] =useState(data)
-  const [index, setIndex] = useState(0)
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
 
   return (
     <section className="section">
@@ -16,10 +16,21 @@ function App() {
       <div className="section-center">
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
-          // more stuff
+
+          // positions
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
 
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
